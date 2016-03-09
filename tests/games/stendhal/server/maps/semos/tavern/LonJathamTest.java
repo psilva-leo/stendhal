4fd1c6a9858d804cccc96f48c27816f038ed4d05
Jack Evans <jack.evans-5@studentmail.manchester.ac.uk>
2016-03-09 13:01:08 +0000

    Implemented code behind Lon Jatham, ready to add new Sprite for him

--- tests/games/stendhal/server/maps/semos/tavern/LonJathamTest.java
+++ tests/games/stendhal/server/maps/semos/tavern/LonJathamTest.java
@@ -61,7 +61,7 @@ public class LonJathamTest extends ZonePlayerAndNPCTestImpl {
 		final Engine en = npc.getEngine();
 
 		assertTrue(en.step(player, "hello"));
-		assertEquals("Good Morning!!! Have you got questions about #java? ", getReply(npc));
+		assertEquals("Good Morning!!! Have you got questions about #java?", getReply(npc));
 
 		assertTrue(en.step(player, "bye"));
 		assertEquals("Bye bye!", getReply(npc));
@@ -76,13 +76,13 @@ public class LonJathamTest extends ZonePlayerAndNPCTestImpl {
 		final Engine en = npc.getEngine();
 
 		assertTrue(en.step(player, "hi"));
-		assertEquals("Good Morning!!! Have you got questions about #java ?", getReply(npc));
+		assertEquals("Good Morning!!! Have you got questions about #java?", getReply(npc));
 
 		assertTrue(en.step(player, "java"));
-		assertEquals("I am the greatest expert of Java, the technology that underpins our world, to ever have lived! Would you like to learn about #objects or #variables, or perhaps how to use a #filebrowser ?", getReply(npc));
+		assertEquals("I am the greatest expert of Java, the technology that underpins our world, to ever have lived! Would you like to learn about #objects or #variables, or perhaps how to use a #filebrowser?", getReply(npc));
 
 		assertTrue(en.step(player, "objects"));
-		assertEquals("Objects allow variables relating to a single thing, such as me, you, or the entire city, to be clustered together! Good, mm?", getReply(npc));
+		assertEquals("Objects allow #variables relating to a single thing, such as me, you, or the entire city, to be clustered together! Good, mm?", getReply(npc));
 		
 		assertTrue(en.step(player, "variables"));
 		assertEquals("In Java, variables are used to assign values for future reference, and passing between programs. These may occur in many types, including #strings, #integers and #doubles", getReply(npc));
@@ -91,10 +91,10 @@ public class LonJathamTest extends ZonePlayerAndNPCTestImpl {
 		assertEquals("Strings are collections of characters, such as words or sentences. This dialogue is stored in a string right now!", getReply(npc));
 		
 		assertTrue(en.step(player, "integers"));
-		assertEquals("Integers are whole number values, as opposed to #doubles ...", getReply(npc));
+		assertEquals("Integers are whole number values, as opposed to #doubles...", getReply(npc));
 		
 		assertTrue(en.step(player, "doubles"));
-		assertEquals("Doubles may be decimal values, as opposed to #integers ...", getReply(npc));
+		assertEquals("Doubles may be decimal values, as opposed to #integers...", getReply(npc));
 		
 		assertTrue(en.step(player, "filebrowser"));
 		assertEquals("No! NEVER USE A FILE BROWSER!", getReply(npc));
