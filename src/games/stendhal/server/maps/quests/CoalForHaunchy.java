bf038d8a84435683e9ec7f0d90c1d3710af41f38
Jack Evans <jack.evans-5@student.manchester.ac.uk>
2016-04-15 13:03:28 +0100

    Began Adding Groovy Changes (w/Daniel) - removed semicolons, modified method operations, etc.

--- src/games/stendhal/server/maps/quests/CoalForHaunchy.java
+++ src/games/stendhal/server/maps/quests/CoalForHaunchy.java
@@ -39,9 +39,10 @@ import games.stendhal.server.entity.npc.condition.TimePassedCondition;
 import games.stendhal.server.entity.player.Player;
 import games.stendhal.server.maps.Region;
 
-import java.util.ArrayList;
+/*import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+Not needed in Groovy - java.util is imported by default*/
 
 /**
  * QUEST: Coal for Haunchy
@@ -73,15 +74,23 @@ import java.util.List;
  * 
  * @author Vanessa Julius and storyteller
  */
-public class CoalForHaunchy extends AbstractQuest {
+ 
+//Classes are set as public by default in groovy
+class CoalForHaunchy extends AbstractQuest {
 
-	private static final String QUEST_SLOT = "coal_for_haunchy";
+	/*private static final String QUEST_SLOT = "coal_for_haunchy";
+	Groovy doesn't need type definitions*/
+	//Semicolons can also be removed
+	private static final QUEST_SLOT = "coal_for_haunchy"
+	
 
 	// The delay between repeating quests is 48 hours or 2880 minutes
-	private static final int REQUIRED_MINUTES = 2880;
+	//private static final int REQUIRED_MINUTES = 2880;
+    private static final REQUIRED_MINUTES = 2880
 
-	private void offerQuestStep() {
-		final SpeakerNPC npc = npcs.get("Haunchy Meatoch");
+    //Can remove empty brackets following private void offerQuestStep(){
+	private void offerQuestStep {
+		final SpeakerNPC npc = npcs.get("Haunchy Meatoch")
 		
 		// player says quest when he has not ever done the quest before (rejected or just new)
 		npc.add(ConversationStates.ATTENDING,
@@ -89,7 +98,7 @@ public class CoalForHaunchy extends AbstractQuest {
 				new QuestNotStartedCondition(QUEST_SLOT),
 				ConversationStates.QUEST_OFFERED, 
 				"I cannot use wood for this huge BBQ. To keep the heat I need some really old stone coal but there isn't much left. The problem is, that I can't fetch it myself because my steaks would burn then so I have to stay here. Can you bring me 25 pieces of #coal for my BBQ please?",
-				null);
+				null)
 
 		npc.add(
 				ConversationStates.QUEST_OFFERED,
@@ -97,7 +106,7 @@ public class CoalForHaunchy extends AbstractQuest {
 				null,
 				ConversationStates.QUEST_OFFERED,
 				"Coal isn't easy to find. You normally can find it somewhere in the ground but perhaps you are lucky and find some in the old Semos Mine tunnels...",
-				null);
+				null)
 
         // player has completed the quest (doesn't happen here)
 		npc.add(ConversationStates.ATTENDING,
@@ -105,7 +114,7 @@ public class CoalForHaunchy extends AbstractQuest {
 				new QuestCompletedCondition(QUEST_SLOT),
 				ConversationStates.ATTENDING,
 				"I can go on with grilling my tasty steaks now! Thank you!",
-				null);
+				null)
 
 		// player asks about quest which he has done already and he is allowed to repeat it
 		npc.add(ConversationStates.ATTENDING,
@@ -113,28 +122,28 @@ public class CoalForHaunchy extends AbstractQuest {
 				new AndCondition(new TimePassedCondition(QUEST_SLOT, 1, REQUIRED_MINUTES), new QuestStateStartsWithCondition(QUEST_SLOT, "waiting;")),
 				ConversationStates.QUEST_OFFERED,
 				"The last coal you brought me is mostly gone again. Will you bring me some more?",
-				null);
-		
+				null)
+
 		// player asks about quest which he has done already but it is not time to repeat it
 		npc.add(ConversationStates.ATTENDING,
 				ConversationPhrases.QUEST_MESSAGES,
 				new AndCondition(new NotCondition(new TimePassedCondition(QUEST_SLOT, 1, REQUIRED_MINUTES)), new QuestStateStartsWithCondition(QUEST_SLOT, "waiting;")),
 				ConversationStates.ATTENDING,
 				null,
-				new SayTimeRemainingAction(QUEST_SLOT, 1, REQUIRED_MINUTES, "The coal amount behind my counter is still high enough. I will not need more for"));
+				new SayTimeRemainingAction(QUEST_SLOT, 1, REQUIRED_MINUTES, "The coal amount behind my counter is still high enough. I will not need more for"))
 
 		// Player agrees to get the coal, increase 5 karma
 		npc.add(ConversationStates.QUEST_OFFERED,
 				ConversationPhrases.YES_MESSAGES, null,
 				ConversationStates.ATTENDING,
 				"Thank you! If you have found 25 pieces, say #coal to me so I know you have it. I'll be sure to give you a nice and tasty reward.",
-				new SetQuestAndModifyKarmaAction(QUEST_SLOT, "start", 5));
+				new SetQuestAndModifyKarmaAction(QUEST_SLOT, "start", 5))
 
 		// Player says no, they've lost karma.
 		npc.add(ConversationStates.QUEST_OFFERED,
 				ConversationPhrases.NO_MESSAGES, null, ConversationStates.IDLE,
 				"Oh, never mind. I thought you love BBQs like I do. Bye then.",
-				new SetQuestAndModifyKarmaAction(QUEST_SLOT, "rejected", -10.0));
+				new SetQuestAndModifyKarmaAction(QUEST_SLOT, "rejected", -10.0))
 	}
 
 	/*
@@ -143,12 +152,12 @@ public class CoalForHaunchy extends AbstractQuest {
 	 * 
 	 */
 	private void bringCoalStep() {
-		final SpeakerNPC npc = npcs.get("Haunchy Meatoch");
+		final SpeakerNPC npc = npcs.get("Haunchy Meatoch")
 		
-		final List<String> triggers = new ArrayList<String>();
-		triggers.add("coal");
-		triggers.add("stone coal");
-		triggers.addAll(ConversationPhrases.QUEST_MESSAGES);
+		final List<String> triggers = new ArrayList<String>()
+		triggers.add("coal")
+		triggers.add("stone coal")
+		triggers.addAll(ConversationPhrases.QUEST_MESSAGES)
 
 		// player asks about quest or says coal when they are supposed to bring some coal and they have it
 		npc.add(
@@ -170,7 +179,7 @@ public class CoalForHaunchy extends AbstractQuest {
 								npc.say("Thank you!! Take " + Grammar.thisthese(grilledsteakAmount) + " " +
 										Grammar.quantityNumberStrNoun(grilledsteakAmount, "grilled steak") + " from my grill!");
 								new SetQuestAndModifyKarmaAction(getSlotName(), "waiting;" 
-										+ System.currentTimeMillis(), 10.0).fire(player, sentence, npc);
+										+ System.currentTimeMillis(), 10.0).fire(player, sentence, npc)
 							}
 						}));
 
@@ -180,82 +189,77 @@ public class CoalForHaunchy extends AbstractQuest {
 				new AndCondition(new QuestInStateCondition(QUEST_SLOT, "start"), new NotCondition(new PlayerHasItemWithHimCondition("coal",25))),
 				ConversationStates.ATTENDING,
 				"You don't have the coal amount which I need yet. Go and pick some more pieces up, please.",
-				null);
+				null)
 		
 		npc.add(
 				ConversationStates.ATTENDING, 
 				Arrays.asList("coal","stone coal"),
 				new QuestNotInStateCondition(QUEST_SLOT,"start"),
 				ConversationStates.ATTENDING,
-				"Sometime you could do me a #favour ...", null);
+				"Sometime you could do me a #favour ...", null)
 	}
 
 	@Override
-	public void addToWorld() {
+	void addToWorld() {
 		fillQuestInfo(
 				"Coal for Haunchy",
 				"Haunchy Meatoch is afraid of his BBQ grillfire. Will his coal last till the steaks are ready or will he need some more?",
-				true);
-		offerQuestStep();
-		bringCoalStep();
+				true)
+		offerQuestStep()
+		bringCoalStep()
 	}
 
 
 	@Override
-	public List<String> getHistory(final Player player) {
+	List<String> getHistory(final Player player) {
 		final List<String> res = new ArrayList<String>();
 		if (!player.hasQuest(QUEST_SLOT)) {
-			return res;
+			return res
 		}
 		res.add("Haunchy Meatoch welcomed me to the Ados market.");
 		final String questState = player.getQuest(QUEST_SLOT);
 		if ("rejected".equals(questState)) {
-			res.add("He asked me to fetch him some pieces of coal but I don't have time to collect some.");
+			res.add("He asked me to fetch him some pieces of coal but I don't have time to collect some.")
 		}
 		if (player.isQuestInState(QUEST_SLOT, "start") || isCompleted(player)) {
-			res.add("The BBQ grill-heat is low and I promised Haunchy to help him out with 25 pieces of coal.");
+			res.add("The BBQ grill-heat is low and I promised Haunchy to help him out with 25 pieces of coal.")
 		}
 		if ("start".equals(questState) && player.isEquipped("coal",25) || isCompleted(player)) {
-			res.add("I found 25 pieces of coal for the Haunchy and think he will be happy.");
+			res.add("I found 25 pieces of coal for the Haunchy and think he will be happy.")
 		}
 		if (isCompleted(player)) {
 			if (isRepeatable(player)) {
-				res.add("I took 25 pieces of coal to the Haunchy, but I'd bet his amount is low again and needs more. Maybe I'll get more grilled tasty steaks.");
+				res.add("I took 25 pieces of coal to the Haunchy, but I'd bet his amount is low again and needs more. Maybe I'll get more grilled tasty steaks.")
 			} else {
-				res.add("Haunchy Meatoch was really happy when I gave him the coal, he has enough for now. He gave me some of the best steaks which I ever ate!");
+				res.add("Haunchy Meatoch was really happy when I gave him the coal, he has enough for now. He gave me some of the best steaks which I ever ate!")
 			}			
 		}
-		return res;
+		res
 	}
 
 	@Override
-	public String getSlotName() {
-		return QUEST_SLOT;
+	String getSlotName {QUEST_SLOT}
+	//Removed public, return, semicolon, empty bracket
+	//Public String getSlotName() {return QUEST_SLOT;}
+
+	@Override
+	String getName {"CoalForHaunchy"}
+
+	@Override
+	boolean isRepeatable(final Player player) {
+		new AndCondition(new QuestStateStartsWithCondition(QUEST_SLOT,"waiting;"),
+				new TimePassedCondition(QUEST_SLOT, 1, REQUIRED_MINUTES)).fire(player,null, null)
 	}
 
 	@Override
-	public String getName() {
-		return "CoalForHaunchy";
-	}
-
-	@Override
-	public boolean isRepeatable(final Player player) {
-		return new AndCondition(new QuestStateStartsWithCondition(QUEST_SLOT,"waiting;"),
-				new TimePassedCondition(QUEST_SLOT, 1, REQUIRED_MINUTES)).fire(player,null, null);
-	}
-
-	@Override
-	public boolean isCompleted(final Player player) {
-		return new QuestStateStartsWithCondition(QUEST_SLOT,"waiting;").fire(player, null, null);
+	boolean isCompleted(final Player player) {
+		new QuestStateStartsWithCondition(QUEST_SLOT,"waiting;").fire(player, null, null)
 	}
 	
 	@Override
-	public String getRegion() {
-		return Region.ADOS_CITY;
-	}
+	String getRegion {Region.ADOS_CITY}
+	//Removed public, empty brackets, return, and semicolon
 
 	@Override
-	public String getNPCName() {
-		return "Haunchy Meatoch";
-	}
+	String getNPCName {"Haunchy Meatoch"}
 }
